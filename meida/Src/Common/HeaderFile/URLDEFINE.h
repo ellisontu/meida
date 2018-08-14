abb3dac17c4127a226e2739065afb0129c88d71f
tdd <tudongdong@@ymfashion.ltd>
2018-08-14 19:15:42 +0800

    add api

--- meida/Src/Common/HeaderFile/URLDEFINE.h
+++ meida/Src/Common/HeaderFile/URLDEFINE.h
@@ -55,7 +55,7 @@
  *  用户注册
  *  @method POST
  *  @link /user/reg
- *  @param: phone / password
+ *  @param: @{phone: 电话号码, password: 密码}
  *  @return json
  */
 //egister
@@ -65,7 +65,7 @@
  *  用户登录
  *  @method POST
  *  @link /user/reg
- *  @param: phone / password
+ *  @param: @{phone=电话号码, password=密码}
  *  @return json
  */
 //egister
@@ -75,7 +75,7 @@
  *  用户获取验证码
  *  @method POST
  *  @link /user/sendSms
- *  @param: phone
+ *  @param: @{phone=电话号码}
  *  @return json
  */
 #define URL_POST_USER_SENSMS [BASEURL stringByAppendingString:@"/user/sendSms"]
@@ -84,7 +84,7 @@
  *  关注
  *  @method POST
  *  @link /user/praise
- *  @param: toUserId(用关注的用户ID) / userId(当前登录用户ID)
+ *  @param: @{toUserId=用关注的用户ID, userId=当前登录用户ID}
  *  @return json
  */
 #define URL_POST_USER_PRAISE [BASEURL stringByAppendingString:@"/user/praise"]
@@ -95,12 +95,11 @@
  *  潮流 -- 专题 list
  *  @method GET
  *  @link /subject/list
- *  @param: currentPage / pageSize
+ *  @param: @{currentPage=当前页, pageSize=请求页数}
  *  @return json
  */
 #define URL_GET_SUBJECT_LIST [BASEURL stringByAppendingString:@"/subject/list"]
 
-//GET /subject/view/{id}/{displayType}
 
 /**
  *  专题详情
@@ -111,6 +110,32 @@
  */
 #define URL_GET_SUBJECT_DETAIL [BASEURL stringByAppendingString:@"/subject/view/%@/%@"]
 
+/**
+ *  上新-查询附近50公里的门店-列表
+ *  @method GET
+ *  @link   /store/list
+ *  @param: @{currentPage=当前页, pageSize=每页显示条数, mylng=我当前位置的经度, mylat=我当前位置的纬度}
+ *  @return json
+ */
+#define URL_GET_STORE_LIST [BASEURL stringByAppendingString:@"/store/list"]
+
+/**
+ *  上新-门店点赞
+ *  @method POST
+ *  @link   /store/praise
+ *  @param: @{storeId=门店ID,  userId=当前登录用户ID}
+ *  @return json
+ */
+#define URL_GET_STORE_PRAISE [BASEURL stringByAppendingString:@"/store/praise"]
+
+/**
+ *  上新-查询门店上新详情
+ *  @method GET
+ *  @link   /store/praise
+ *  @param: @{storeId=门店ID, userId=当前登录用户ID}
+ *  @return json
+ */
+#define URL_GET_STORE_SNEW [BASEURL stringByAppendingString:@"/store/snew"]
 
 
 #pragma mark ------------------------------------   评论 comment   ----------------------------------
@@ -119,7 +144,7 @@
  *  查询评论列表，根据关联ID
  *  @method GET
  *  @link /comment/queryListByRefId
- *  @param: refId 关联id
+ *  @param: @{refId=关联id}
  *  @return json
  */
 #define URL_GET_COMMENT_QUERY_LIST [BASEURL stringByAppendingString:@"/comment/queryListByRefId"]
@@ -128,7 +153,7 @@
  *  新增评论
  *  @method POST
  *  @link /comment/add
- *  @param: refId(关联id) / content(评论内容) / userId(当前登录用户ID)
+ *  @param: @{refId=关联id, content=评论内容, userId=当前登录用户ID}
  *  @return json
  */
 #define URL_GET_COMMENT_ADD [BASEURL stringByAppendingString:@"/comment/add"]
@@ -137,10 +162,173 @@
  *  删除评论
  *  @method POST
  *  @link /comment/del/{id}
- *  @param: id(关联id)
+ *  @param: @{id=关联id}
  *  @return json
  */
 #define URL_GET_COMMENT_DELETE [BASEURL stringByAppendingString:@"/comment/del/%@"]
+
+
+#pragma mark ------------------------------------   衣橱    ----------------------------------
+
+/**
+ *  新增衣物
+ *  @method POST
+ *  @link /goods/add
+ *  @param: @{名称=goodsName,商品配图1=pic1,商品配图2=pic2,商品配图3=pic3,分类=goodsTypeId,颜色=colors,适用季节=season,
+    场合=occasions,标签=tags,收纳位置=takeInLocation,版型指数= stereotype,品牌=brandId,商家=orgId,价格=pirce(默认为0.00元),
+    主要材质=material,洗护=wash,货品描述=description,上市时间=marketDate,补充语音录入=soundUrl,登录用户ID=userId}
+ *  @return json
+ */
+#define URL_GET_GOODS_ADD [BASEURL stringByAppendingString:@"/goods/add"]
+
+/**
+ *  新增我的衣橱分类
+ *  @method POST
+ *  @link /goods/addMyGoodsType
+ *  @param: @{userId=当前登录用户ID, name=衣橱分类名称}
+ *  @return json
+ */
+#define URL_GET_GOODS_ADD_MYGOODS_TYPE [BASEURL stringByAppendingString:@"/goods/addMyGoodsType"]
+
+/**
+ *  删除我的衣橱分类
+ *  @method POST
+ *  @link /goods/addMyGoodsType
+ *  @param: @{goodsTypeId=衣橱分类ID}
+ *  @return json
+ */
+#define URL_GET_GOODS_DEL_MYGOODS_TYPE [BASEURL stringByAppendingString:@"/goods/delMyGoodsType"]
+
+/**
+ *  查询品牌列表
+ *  @method GET
+ *  @link /goods/getBrands
+ *  @param: nil
+ *  @return json
+ */
+#define URL_GET_GOODS_GETBRANDS [BASEURL stringByAppendingString:@"/goods/getBrands"]
+
+/**
+ *  查询品牌列表
+ *  @method GET
+ *  @link /goods/getBrands
+ *  @param: nil
+ *  @return json
+ */
+#define URL_GET_GOODS_GETBRANDS [BASEURL stringByAppendingString:@"/goods/getBrands"]
+
+/**
+ *  根据衣橱分类查询衣物
+ *  @method POST
+ *  @link  /goods/getGoodsByTypeId/{goodsTypeId}
+ *  @param: @{goodsTypeId= 衣橱分类, userId=当前登录用户ID}
+ *  @return json
+ */
+#define URL_GET_GOODS_GETGOODS_BYTYPEID [BASEURL stringByAppendingString:@"/goods/getGoodsByTypeId/%@"]
+
+/**
+ *  查询商家列表
+ *  @method GET
+ *  @link  /goods/getOrgs
+ *  @param: nil
+ *  @return json
+ */
+#define URL_GET_GOODS_GETORGS [BASEURL stringByAppendingString:@"/goods/getOrgs"]
+
+/**
+ *  查询我的衣橱分类
+ *  @method GET
+ *  @link  /goods/myGoodsTypes
+ *  @param: @{userId=当前登录用户ID}
+ *  @return json
+ */
+#define URL_GET_GOODS_MYGOODSTYPE [BASEURL stringByAppendingString:@"/goods/myGoodsTypes"]
+
+/**
+ *  查询我的衣橱分类
+ *  @method GET
+ *  @link  /goods/myGoodsTypes
+ *  @param: @{userId=当前登录用户ID}
+ *  @return json
+ */
+#define URL_GET_GOODS_MYGOODSTYPE [BASEURL stringByAppendingString:@"/goods/myGoodsTypes"]
+
+/**
+ *  衣物点赞
+ *  @method POST
+ *  @link  /goods/praise
+ *  @param: @{goodsId=衣物ID, userId=当前登录用户ID}
+ *  @return json
+ */
+#define URL_GET_GOODS_PRAISE [BASEURL stringByAppendingString:@"/goods/praise"]
+
+/**
+ *  衣物点赞
+ *  @method POST
+ *  @link  /goods/praise
+ *  @param: @{goodsId=衣物ID, userId=当前登录用户ID}
+ *  @return json
+ */
+#define URL_GET_GOODS_PRAISE [BASEURL stringByAppendingString:@"/goods/praise"]
+
+/**
+ *  修改我的衣橱分类
+ *  @method POST
+ *  @link  /goods/updateMyGoodsType
+ *  @param: @{goodsTypeId=衣橱分类ID, name=衣橱名称}
+ *  @return json
+ */
+#define URL_GET_GOODS_UPDATE_MYGOODSTYPE [BASEURL stringByAppendingString:@"/goods/updateMyGoodsType"]
+
+/**
+ *  查询衣物详情
+ *  @method GET
+ *  @link  /goods/view
+ *  @param: @{userId=当前登录用户ID, goodsId=goodsId}
+ *  @return json
+ */
+#define URL_GET_GOODS_VIEW [BASEURL stringByAppendingString:@"/goods/view"]
+
+
+#pragma mark ------------------------------------   衣橱 -> 搭配   ----------------------------------
+
+/**
+ *  查询我的搭配
+ *  @method POST
+ *  @link  /goodsMatch/add
+ *  @param: @{matchPicUrl=搭配照片，多个以逗号分隔, occasion=场合,season=适用季节, note=心得, userId=登录用户id}
+ *  @return json
+ */
+#define URL_GET_GOODSMATCH_ADD [BASEURL stringByAppendingString:@"/goodsMatch/add"]
+
+/**
+ *  删除我的搭配详情
+ *  @method POST
+ *  @link  /goodsMatch/del/{id}
+ *  @param: @{id=搭配ID}
+ *  @return json
+ */
+#define URL_GET_GOODSMATCH_DELETE [BASEURL stringByAppendingString:@"/goodsMatch/del/%@"]
+
+
+/**
+ *  查询我的搭配列表
+ *  @method GET
+ *  @link  /goodsMatch/list/{userId}
+ *  @param: @{userId=当前登录用户ID}
+ *  @return json
+ */
+#define URL_GET_GOODSMATCH_LIST [BASEURL stringByAppendingString:@"/goodsMatch/list/%@"]
+
+/**
+ *  查询我的搭配详情
+ *  @method GET
+ *  @link  /goodsMatch/view/{id}
+ *  @param: @{id=搭配ID}
+ *  @return json
+ */
+#define URL_GET_GOODSMATCH_VIEW [BASEURL stringByAppendingString:@"/goodsMatch/view/%@"]
+
 
 
 
